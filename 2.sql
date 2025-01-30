df398d62f956b6425e4b243a8a469fecaef7d160
huavcjj <huavcjj0217@icloud.com>
2025-01-30 22:03:29 +0900

    feat

--- 2.sql
+++ 2.sql
@@ -48,3 +48,43 @@ CREATE TABLE users(
  
 DESCRIBE users;
 ------------------------------------------------------------
+
+SELECT DATABASE();
+
+# テーブル一覧
+SHOW TABLES;
+
+# テーブル名変更
+ALTER TABLE users RENAME TO users_table;
+
+DESCRIBE users_table;
+
+# カラムの削除
+ALTER TABLE users_table DROP COLUMN message;
+
+# カラムの追加
+ALTER TABLE users_table ADD post_code CHAR(8);
+
+ALTER TABLE users_table ADD gender CHAR(1) AFTER age;
+
+# 一番最初のカラムに追加
+ALTER TABLE users_table ADD new_id INT FIRST;
+
+
+ALTER TABLE users_table DROP COLUMN new_id;
+
+# カラムの定義変更
+ALTER TABLE users_table MODIFY name VARCHAR(50);
+
+# カラム名を変更
+ALTER TABLE users_table CHANGE COLUMN name 名前 VARCHAR(50);
+
+DESCRIBE users_table;
+
+ALTER TABLE users_table CHANGE COLUMN gender gender CHAR(1) AFTER post_code;
+
+
+# 主キーを削除
+ALTER TABLE users_table DROP PRIMARY KEY;
+
+------------------------------------------------------------
