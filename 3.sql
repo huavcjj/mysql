445357f253a8b5601de24d2cf16c877bb0ce3840
huavcjj <huavcjj0217@icloud.com>
2025-01-31 19:50:49 +0900

    feat

--- 3.sql
+++ 3.sql
@@ -19,6 +19,34 @@ CREATE TABLE people(
  INSERT INTO people VALUES(4,"Jike's son","2020-01-13");
  
  SELECT * FROM people;
- 
- ------------------------------------------------------------
- 
+
+------------------------------------------------------------
+SHOW TABLES;
+
+SELECT * FROM people;
+
+SELECT name,id, birth_day,name FROM people;
+
+SELECT id AS "番号", name AS "名前" FROM people;
+
+SELECT * FROM people WHERE id=3;
+
+SELECT * FROM people WHERE id>2;
+
+UPDATE people SET birth_day="1200-01-02",name="";
+
+SELECT * FROM people;
+
+UPDATE people SET name="Taro", birth_day="2003-04-27" WHERE id=3;
+
+UPDATE people SET name="Joro", birth_day="2000-07-11" WHERE id>4;
+
+DELETE FROM people WHERE id>4;
+
+DELETE FROM people;
+
+SHOW TABLES;
+
+SELECT * FROM people;
+------------------------------------------------------------
+
