cc750135a9d6a2dbf0a5083675ad916925e7a1b4
huavcjj <huavcjj0217@icloud.com>
2025-01-31 20:48:47 +0900

    feat

--- 4.sql
+++ 4.sql
@@ -32,3 +32,33 @@ SELECT * FROM users WHERE id=1;
 SELECT * FROM users ORDER BY id DESC LIMIT 1;
 
 DELETE FROM users WHERE id>190;
+------------------------------------------------------------
+SELECT DATABASE();
+
+DESCRIBE customers;
+
+SELECT * FROM customers WHERE name IS NULL;
+
+SELECT NULL IS NULL;
+
+SELECT * FROM customers WHERE name IS NOT NULL;
+
+SELECT * FROM prefectures;
+
+SELECT * FROM prefectures WHERE name IS NULL;
+
+SELECT * FROM prefectures WHERE name="";
+
+
+SELECT * FROM users WHERE age NOT BETWEEN 5 AND 10;
+
+SELECT * FROM users WHERE age BETWEEN 10 AND 20;
+
+
+SELECT * FROM users WHERE name LIKE "村%";
+
+SELECT * FROM users WHERE name LIKE "%郎";
+
+SELECT * FROM users WHERE name LIKE "%ab%";
+
+SELECT * FROM prefectures WHERE name LIKE "福_県" ORDER BY name;
