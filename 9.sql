20a79016decfb356e50d1c09055cdda881a4275b
huavcjj <huavcjj0217@icloud.com>
2025-02-01 19:05:36 +0900

    feat

--- 9.sql
+++ 9.sql
@@ -56,5 +56,44 @@ SELECT * FROM students WHERE id < 10
 EXCEPT
 SELECT * FROM new_students WHERE id < 10;
 
+------------------------------------------------------------
+SELECT * FROM customers WHERE name IS NULL;
+
+-- COUNT
+
+SELECT COUNT(*) FROM customers; -- NULLをカウント
+
+SELECT COUNT(name) FROM customers; -- NULLをカウントしない
+
+SELECT COUNT(name) FROM customers WHERE id > 80;
+
+-- MAX, MIN
+
+SELECT MAX(age), MIN(age) FROM users WHERE birth_place = "日本";
+
+SELECT MAX(birth_day), MIN(birth_day) FROM users;
+
+-- SUM
+
+SELECT SUM(salary) FROM employees;
+
+-- AVG (NULLの場合面倒)
+
+SELECT AVG(salary) FROM employees;
+
+
+CREATE TABLE tmp_count(
+	num INT
+);
+
+INSERT INTO tmp_count VALUES(1);
+INSERT INTO tmp_count VALUES(2);
+INSERT INTO tmp_count VALUES(3);
+INSERT INTO tmp_count VALUES(NULL);
+
+SELECT * FROM tmp_count;
+
+SELECT AVG(COALESCE(num,0)) FROM tmp_count;
+
 
 
