a52e84a48b72cb12a0fedfd45fdd99bf8893de4f
huavcjj <huavcjj0217@icloud.com>
2025-02-02 23:56:21 +0900

    feat

--- 10.sql
+++ 10.sql
@@ -46,3 +46,59 @@ WHERE age > (
     SELECT AVG(age) 
     FROM employees
 );
+
+------------------------------------------------------------
+-- FROM
+
+SELECT department_id, AVG(age) AS avg_age FROM employees GROUP BY department_id;
+
+
+SELECT 
+    MAX(avg_age) AS "部署ごとの平均年齢の最大",
+    MIN(avg_age) AS "部署ごとの平均年齢の最小"
+FROM (
+    SELECT 
+        department_id, 
+        AVG(age) AS avg_age 
+    FROM employees 
+    GROUP BY department_id
+) AS tmp_emp;
+
+
+SELECT 
+    MAX(age_count) AS "最大年齢グループ人数", 
+    MIN(age_count) AS "最小年齢グループ人数"
+FROM (
+    SELECT 
+        FLOOR(age / 10) * 10 AS age_group, 
+        COUNT(*) AS age_count 
+    FROM employees 
+    GROUP BY FLOOR(age / 10)
+) AS age_summary;
+
+
+-- SELECT
+
+
+SELECT * FROM customers;
+
+SELECT * FROM orders;
+
+
+SELECT 
+    cs.first_name,
+    cs.last_name,
+    (
+        SELECT MAX(order_date) 
+        FROM orders AS order_max 
+        WHERE cs.id = order_max.customer_id
+    ) AS "最近の注文日",
+    (
+		SELECT SUM(order_amount * order_price)
+		FROM orders AS tmp_order
+		WHERE cs.id = tmp_order.customer_id
+    ) AS "今までのお支払い金額"
+FROM customers AS cs
+WHERE cs.id < 10;
+
+------------------------------------------------------------
