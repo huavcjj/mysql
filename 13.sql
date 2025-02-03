c53f3b1d3db37b82e6945b7c76639f309c510edc
huavcjj <huavcjj0217@icloud.com>
2025-02-03 21:15:13 +0900

    feat

---
+++ 13.sql
@@ -0,0 +1,39 @@
+SELECT * FROM employees;
+
+-- WINDOWS関数
+
+-- PARTITION BY
+
+SELECT
+    *,
+    AVG(age) OVER() AS avg_age,
+    COUNT(*) OVER() AS total_count
+FROM employees;
+
+SELECT
+	*,
+	AVG(age) OVER(PARTITION BY department_id) AS avg_age,
+	COUNT(*) OVER(PARTITION BY department_id) AS total_count
+FROM employees;
+
+
+
+SELECT
+	*,
+	COUNT(*) OVER(PARTITION BY FLOOR(age/10)) AS age_count, 
+	FLOOR(age/10)
+FROM employees;
+
+SELECT 
+    *,
+    SUM(order_amount * order_price) OVER(PARTITION BY DATE_FORMAT(order_date, "%Y/%m")) AS monthly_sales
+FROM orders;
+
+
+
+
+
+
+
+
+
