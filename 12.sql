9537dfd4943d71a8628e13b3d8550e55a0f667f1
huavcjj <huavcjj0217@icloud.com>
2025-02-03 20:28:00 +0900

    feat

--- 12.sql
+++ 12.sql
@@ -231,3 +231,54 @@ CROSS JOIN (
 	FROM salaries
 ) AS summary
 GROUP BY emp.id, summary.avg_payment
+
+------------------------------------------------------------
+
+-- WITH
+
+SELECT
+	*
+FROM employees AS e
+INNER JOIN departments AS d
+ON e.department_id = d.id
+WHERE d.name = "営業部";
+
+
+WITH tmp_departments AS (
+	SELECT 
+		*
+	FROM departments
+	WHERE name = "営業部"
+)
+SELECT
+	*
+FROM employees AS e
+INNER JOIN tmp_departments
+	ON e.department_id = tmp_departments.id;
+	
+
+WITH tmp_stores AS (
+    SELECT
+        *
+    FROM stores
+    WHERE id IN (1, 2, 3)
+), tmp_items_orders AS (
+    SELECT 
+        items.id AS item_id,
+        tmp_stores.id AS store_id,
+        orders.id AS order_id,
+        orders.order_amount AS order_amount,
+        orders.order_price AS order_price,
+        tmp_stores.name AS store_name
+    FROM tmp_stores
+    INNER JOIN items
+        ON tmp_stores.id = items.store_id
+    INNER JOIN orders
+        ON items.id = orders.item_id
+)
+SELECT 
+    store_name, 
+    SUM(order_amount * order_price) AS total_sales
+FROM tmp_items_orders
+GROUP BY store_name;
+
