6fed9b2f2af6b278b20cf65085bdbccd3bb80739
huavcjj <huavcjj0217@icloud.com>
2025-02-03 19:37:54 +0900

    feat

--- 12.sql
+++ 12.sql
@@ -84,3 +84,92 @@ RIGHT JOIN enrollments AS enr
 ON std.id = enr.student_id
 RIGHT JOIN classes AS cs
 ON enr.class_id = cs.id;
+
+
+------------------------------------------------------------
+-- customers, orders, items, stores を紐づける
+-- customers.id で並び替える
+
+SELECT
+    ct.id,
+    ct.last_name,
+    od.item_id,
+    od.order_amount,
+    od.order_price,
+    od.order_date,
+    it.name AS item_name,
+    st.name AS store_name
+FROM 
+    customers AS ct
+INNER JOIN 
+    orders AS od ON ct.id = od.customer_id
+INNER JOIN 
+    items AS it ON od.item_id = it.id
+INNER JOIN 
+    stores AS st ON it.store_id = st.id
+ORDER BY 
+    ct.id;
+
+-- customers, orders, items, stores を紐づける
+-- customers.id で並び替える
+-- customers.idが10で, orders.order_date が 2020-08-01より後に絞り込む
+
+SELECT
+    ct.id,
+    ct.last_name,
+    od.item_id,
+    od.order_amount,
+    od.order_price,
+    od.order_date,
+    it.name AS item_name,
+    st.name AS store_name
+FROM 
+    customers AS ct
+INNER JOIN 
+    orders AS od ON ct.id = od.customer_id
+INNER JOIN 
+    items AS it ON od.item_id = it.id
+INNER JOIN 
+    stores AS st ON it.store_id = st.id
+WHERE 
+    ct.id = 10
+    AND od.order_date > '2020-08-01'
+ORDER BY 
+    ct.id;
+
+SELECT
+    ct.id,
+    ct.last_name,
+    od.item_id,
+    od.order_amount,
+    od.order_price,
+    od.order_date,
+    it.name AS item_name,
+    st.name AS store_name
+FROM 
+    (SELECT * FROM customers WHERE id = 10) AS ct
+INNER JOIN 
+    (SELECT * FROM orders WHERE order_date > '2020-08-01') AS od
+    ON ct.id = od.customer_id
+INNER JOIN 
+    items AS it
+    ON od.item_id = it.id
+INNER JOIN 
+    stores AS st
+    ON it.store_id = st.id
+ORDER BY 
+    ct.id;
+   
+-- GROUP BY の紐付け
+SELECT * 
+FROM customers AS ct
+INNER JOIN (
+    SELECT 
+        customer_id, 
+        SUM(order_amount * order_price) AS summary_price
+    FROM orders
+    GROUP BY customer_id
+) AS order_summary 
+ON ct.id = order_summary.customer_id
+ORDER BY ct.age
+LIMIT 10;
