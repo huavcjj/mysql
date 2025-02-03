5e2cf926b8bd2909b2957e5b49a22070b2ef18f5
huavcjj <huavcjj0217@icloud.com>
2025-02-03 17:38:22 +0900

    feat

--- 11.sql
+++ 11.sql
@@ -34,3 +34,23 @@ WHERE EXISTS (
     WHERE dt.name IN ('営業部', '開発部')
     AND em.department_id = dt.id
 );
+------------------------------------------------------------
+
+SELECT * 
+FROM customers AS cd
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM orders AS od
+    WHERE cd.id = od.customer_id
+    AND od.order_date = '2020-12-31'
+);
+
+
+SELECT * 
+FROM employees AS em1
+WHERE EXISTS (
+	SELECT 1
+	FROM employees AS em2
+	WHERE em1.manager_id = em2.id
+);
+
