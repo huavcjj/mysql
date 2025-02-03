fa20708014248364d8721caeadb9a3b4e19aad4d
huavcjj <huavcjj0217@icloud.com>
2025-02-03 22:06:16 +0900

    feat

--- 13.sql
+++ 13.sql
@@ -46,8 +46,64 @@ SELECT
 	FLOOR(age / 10) * 10,
 	COUNT(*) OVER(ORDER BY FLOOR(age / 10))
 FROM employees;
+------------------------------------------------------------
+
+-- PARTITION BY, ORDER BY
+
+SELECT
+	*,
+	MIN(age) OVER(PARTITION BY department_id ORDER BY age) AS count_value
+FROM employees;
+
+SELECT 
+	*,
+	SUM(sa.payment) OVER(PARTITION BY sa.paid_date ORDER BY emp.id)
+FROM employees AS emp
+INNER JOIN salaries AS sa
+ON emp.id = sa.employee_id;
 
 
+-- ROWS BETWEEN : 行を基準
+
+-- UNBOUNDED PRECEDING : 一番頭の行
+-- n PRECEDING : 現在の行よりn行前の行
+-- CURRENT ROW : 現在の行
+-- n FOLLOWING : 現在の行よりn行後の行
+-- UNBOUNDED FOLLOWIGN : 一番最後の行
+
+-- RANGE BETWEEN : 値を基準
+
+
+WITH daily_summary AS (
+	SELECT 
+		order_date,
+		SUM(order_price * order_amount) AS sale
+	FROM orders
+	GROUP BY order_date
+)
+SELECT 
+	*,
+	AVG(sale) OVER(ORDER BY order_date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)
+FROM daily_summary;
+
+
+
+SELECT 
+    emp.*,
+    summary_salary.payment,
+    SUM(summary_salary.payment) OVER(
+        ORDER BY emp.age 
+        RANGE BETWEEN 3 PRECEDING AND CURRENT ROW
+    ) AS p_summary
+FROM employees AS emp
+INNER JOIN (
+    SELECT 
+        employee_id,
+        SUM(payment) AS payment
+    FROM salaries
+    GROUP BY employee_id
+) AS summary_salary
+ON emp.id = summary_salary.employee_id;
 
 
 
