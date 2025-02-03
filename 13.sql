13fe025234ac5903e2db2f9545a449b2cbc68eed
huavcjj <huavcjj0217@icloud.com>
2025-02-03 21:28:10 +0900

    feat

--- 13.sql
+++ 13.sql
@@ -29,7 +29,23 @@ SELECT
     SUM(order_amount * order_price) OVER(PARTITION BY DATE_FORMAT(order_date, "%Y/%m")) AS monthly_sales
 FROM orders;
 
+------------------------------------------------------------
+-- ORDER BY
 
+SELECT
+	*,
+	COUNT(*) OVER(ORDER BY age) AS tmp_count
+FROM employees;
+
+SELECT 
+	*,
+	SUM(order_price) OVER(ORDER BY order_date, customer_id) 
+FROM orders;
+
+SELECT
+	FLOOR(age / 10) * 10,
+	COUNT(*) OVER(ORDER BY FLOOR(age / 10))
+FROM employees;
 
 
 
