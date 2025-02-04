0a6e272a1173a824c6971c2656533594836c9174
huavcjj <huavcjj0217@icloud.com>
2025-02-04 14:53:52 +0900

    feat

--- 14.sql
+++ 14.sql
@@ -49,4 +49,49 @@ GROUP BY
         ELSE 'クラス2'
     END;
     
-  
+  SELECT 
+    emp.id,
+	SUM(sa.payment),
+    AVG(sa.payment)
+FROM employees AS emp
+INNER JOIN salaries AS sa
+    ON emp.id = sa.employee_id
+WHERE emp.age < 40
+GROUP BY emp.id
+HAVING AVG(sa.payment) > 7000000;
+
+
+SELECT * FROM customers;
+
+SELECT * FROM orders;
+
+SELECT 
+    cst.*,
+    (SELECT SUM(od.order_amount) 
+     FROM orders AS od
+     WHERE od.customer_id = cst.id) AS sum_order_amount
+FROM customers AS cst;
+
+SELECT 
+    CONCAT(customers.last_name, customers.first_name) AS full_name,
+    COUNT(*) AS order_count
+FROM (
+    SELECT *
+    FROM customers
+    WHERE last_name LIKE "%田%"
+) AS customers
+INNER JOIN (
+    SELECT * 
+    FROM orders 
+    WHERE order_date >= "2020-12-01"
+) AS orders
+    ON customers.id = orders.customer_id
+INNER JOIN items
+    ON orders.item_id = items.id
+INNER JOIN (
+    SELECT *
+    FROM stores
+    WHERE name = "山田商店"
+) AS stores
+    ON stores.id = items.store_id
+GROUP BY CONCAT(customers.last_name, customers.first_name);
