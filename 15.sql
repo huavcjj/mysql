a47f864c4b66ff233b894a39cd8c44560927cace
huavcjj <huavcjj0217@icloud.com>
2025-02-04 16:38:47 +0900

    feat

--- 15.sql
+++ 15.sql
@@ -43,3 +43,71 @@ LEFT JOIN departments AS dt
 SET emp.department_name = COALESCE(dt.name,'不明');
 
 SELECT * FROM employees;
+
+-- WITH UPDATE
+
+SELECT * FROM stores;
+
+ALTER TABLE stores
+ADD all_sales INT;
+
+SELECT * FROM stores;
+
+SELECT * FROM items;
+
+SELECT * FROM orders;
+
+
+WITH tmp_sales AS (
+	SELECT 
+		it.store_id,
+		SUM(od.order_amount * od.order_price) AS summary
+	FROM items AS it
+	INNER JOIN orders AS od
+		ON it.id = od.item_id
+	GROUP BY it.store_id
+)
+UPDATE stores AS st
+INNER JOIN tmp_sales AS ts
+	ON st.id = ts.store_id
+SET st.all_sales = ts.summary;
+
+SELECT * FROM stores;
+
+
+-- DELETE
+
+DELETE FROM employees
+WHERE department_id IN(
+	SELECT id
+	FROM departments
+	WHERE name = "開発部"
+);
+
+SELECT * FROM employees;
+
+-- INSERT
+
+SELECT * FROM customers;
+
+SELECT * FROM orders;
+
+
+CREATE TABLE customer_orders(
+	name VARCHAR(255),
+	order_date DATE,
+	sales INT,
+	total_sales INT
+);
+
+INSERT INTO customer_orders
+SELECT
+	CONCAT(ct.last_name,ct.first_name),
+	od.order_date,
+	od.order_amount * od.order_price,
+	SUM(od.order_amount * od.order_price) OVER(PARTITION BY CONCAT(ct.last_name,ct.first_name) ORDER BY od.order_date)
+FROM customers AS ct
+INNER JOIN orders AS od
+	ON ct.id = od.customer_id;
+
+SELECT * FROM customer_orders;
